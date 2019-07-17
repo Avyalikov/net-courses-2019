a583500060c5428b0d4eec267434001ef0e8fb2f
Foxtra <tkachew.sergey@ya.ru>
2019-07-17 21:19:31 +0300

    All logic is added to separate class 'Game'. MaxLeve added. Exit button added.

--- HW1/Sergey_Tkachev/DoorsAndLevels/DoorsAndLevels/Game.cs
+++ HW1/Sergey_Tkachev/DoorsAndLevels/DoorsAndLevels/Game.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Game
+namespace DoorsAndLevels
 {
-    class Program
+    class Game
     {
+        int[] levelNumbers;
+        Stack<int> history;
+        int selectedNum;
+        int maxLevel;
+        int currentLevel;
+        int exitNum;
+
+        public Game()
+        {
+            this.levelNumbers = new int[] { 0, 0, 0, 0, 0 };
+            this.history = new Stack<int>();
+            this.maxLevel = 6;
+            exitNum = -1;
+            currentLevel = 1;
+        }
+
         /// <summary>Fills array with unique numbers from 1 to 7 except the last element.</summary>
         /// <param name="nums">Array to fill</param>
-        static void FillArray(ref int[] nums)
+        void FillArray(ref int[] nums)
         {
             Random random = new Random();
             int num;
             for (int i = 0; i < nums.Length - 1; i++)
             {
-                do{
+                do
+                {
                     num = random.Next(1, 7);
                 } while (ContainsInArray(ref nums, num));
                 nums[i] = num;
@@ -22,7 +42,7 @@ namespace Game
 
         /// <summary>Prints array into console.</summary>
         /// <param name="nums">Array of integers to print</param>
-        static void printArray(ref int[] nums)
+        void printArray(ref int[] nums)
         {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -33,7 +53,7 @@ namespace Game
 
         /// <summary>Checks if entered number is integer, if not then number should be entered again.</summary>
         /// <returns></returns>
-        static int GetNum()
+        int GetNum()
         {
             while (true)
                 if (!int.TryParse(Console.ReadLine(), out int enteredNum))
@@ -45,7 +65,7 @@ namespace Game
         /// <param name="nums">Array to check</param>
         /// <param name="num">Number to find</param>
         /// <returns></returns>
-        static bool ContainsInArray(ref int[] nums, int num)
+        bool ContainsInArray(ref int[] nums, int num)
         {
             for (int i = 0; i < nums.Length; i++)
                 if (num == nums[i])
@@ -56,7 +76,7 @@ namespace Game
         /// <summary>Divides all elements of array to a number.</summary>
         /// <param name="nums">Array of integers</param>
         /// <param name="denominator">Denominator</param>
-        static void DivideArrayElements(ref int[] nums, int denominator)
+        void DivideArrayElements(ref int[] nums, int denominator)
         {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -67,7 +87,7 @@ namespace Game
         /// <summary>Multiplies all elements of array to a number.</summary>
         /// <param name="nums">Array of integers</param>
         /// <param name="multiplier">Multiplier</param>
-        static void MultiplyArrayElements(ref int[] nums, int multiplier)
+        void MultiplyArrayElements(ref int[] nums, int multiplier)
         {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -75,44 +95,56 @@ namespace Game
             }
         }
 
-
-        static void Main(string[] args)
+        public void Run()
         {
-            int[] levelNumbers = { 0, 0, 0, 0, 0 };
-            Stack<int> history = new Stack<int>();
-            int selectedNum;
 
             FillArray(ref levelNumbers);
 
-            Console.WriteLine("Welcome to the Doors and Levels game.");
+            Console.WriteLine($"Welcome to the Doors and Levels game. Enter '{exitNum}' to exit.");
 
             while (true)
             {
-                Console.Write("There are the next numbers: ");
+                Console.Write($"Level #{currentLevel}. There are the next doors: ");
 
                 printArray(ref levelNumbers);
 
-                do{
+                do
+                {
                     Console.Write("Select one of existing numbers: ");
                     selectedNum = GetNum();
-
+                    if (selectedNum == exitNum)
+                        break;
                 } while (!ContainsInArray(ref levelNumbers, selectedNum));
 
-                if(selectedNum == 0)
+                if (selectedNum == exitNum)
                 {
-                    if (history.Count > 0)
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+                }
+                else if (selectedNum == 0)
+                {
+                    if (currentLevel > 1)
                     {
                         DivideArrayElements(ref levelNumbers, history.Pop());
                         Console.WriteLine($"You selected {selectedNum} and go to the previous level.");
+                        currentLevel--;
                     }
-                    else
+                    else if (currentLevel == 1) {
                         Console.WriteLine("This is a first level already. Choose another number.");
+                    }
                 }
-                else
+                else 
                 {
-                    MultiplyArrayElements(ref levelNumbers, selectedNum);
-                    history.Push(selectedNum);
-                    Console.WriteLine($"You selected {selectedNum} and go to the next level.");
+                    if (currentLevel < maxLevel)
+                    {
+                        MultiplyArrayElements(ref levelNumbers, selectedNum);
+                        history.Push(selectedNum);
+                        Console.WriteLine($"You selected '{selectedNum}' and go to the next level.");
+                        currentLevel++;
+                    }
+                    else {
+                       Console.WriteLine("You are on the max level now. The only way is back.");
+                    }
                 }
 
             }
