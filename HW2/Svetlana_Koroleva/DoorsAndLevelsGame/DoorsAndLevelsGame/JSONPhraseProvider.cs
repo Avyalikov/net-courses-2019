2d8ba3cce3a0a45daf6553c68d10c91bb1191735
korolsv <koroleva-sa@outlook.com>
2019-07-21 00:11:56 +0300

    Refactoring of the game. Step 1. Phrase provider has been extracted.

---
+++ HW2/Svetlana_Koroleva/DoorsAndLevelsGame/DoorsAndLevelsGame/JSONPhraseProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DoorsAndLevelsGame
+{
+    public class JSONPhraseProvider : IPhraseProvider
+    {
+       public string GetPhrase(string keyword)
+        {
+            var resourceFile = new FileInfo("..\\..\\Resources\\PhrasesRuLang.json");
+
+            if (!resourceFile.Exists)
+            {
+                throw new ArgumentException($"The language file {resourceFile.Name} doesn't exist");
+            }
+            var resourceFileContent = File.ReadAllText(resourceFile.FullName);
+            try
+            {
+                var resourceData = JsonConvert.DeserializeObject<Dictionary<string, string>>(resourceFileContent);
+
+                return resourceData[keyword];
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(
+                    $"There is no element under {keyword}", ex);
+            }
+        }
+    }
+}
---
+++ HW2/Svetlana_Koroleva/DoorsAndLevelsGame/DoorsAndLevelsGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorsAndLevelsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPhraseProvider phraseProvider = new JSONPhraseProvider();
+            Game game = new Game(phraseProvider);
+
+            while (game.Exit==false)
+            {
+                game.PlayGame();               
+               
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
