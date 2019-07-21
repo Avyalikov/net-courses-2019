4447247a5f2754f24370f4242b8b6b3e7b8da9bd
Mikhail Romashov <romashovmvl@gmail.com>
2019-07-21 21:47:36 +0300

    Modified a few methods. Now used only one public method. All the others are private. Added exit code for exit from program.

--- HW2/Mikhail_Romashov/DoorsAndLevels/DoorsAndLevels/DoorsAndLevels.cs
+++ HW2/Mikhail_Romashov/DoorsAndLevels/DoorsAndLevels/DoorsAndLevels.cs
@@ -8,20 +8,46 @@ namespace DoorsAndLevels
 {
     class DoorsAndLevels
     {
-        private int[] m_arrayNum;           //array of numbers
+        private int[] m_arrayDoorsValue;           //array of doors value
         private Stack<int> m_levelCoeff;    //stack witch contains coefficient for each level
+        bool exitCode = false;          //flag to exit (if entered negative number)
+
+        public void Run()
+        {
+            Console.WriteLine("Let`s start to game");
+
+            do
+            {
+                Console.WriteLine("Choose one of the number for next level or \'0\' to previous level.");
+                Console.WriteLine("For exit enter a negative number:");
+                this.Show();
+                string resultStr = Console.ReadLine();
+                try
+                {
+                    int result = Convert.ToInt32(resultStr);
+                    this.CalcLevel(result);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The value '{resultStr}' is not a number.");
+                }
+
+            } while (!exitCode);
+            Console.WriteLine("Thank you for playing! Enter any key to exit.");
+            Console.ReadKey();
+        }
 
         public DoorsAndLevels()
         {
-            m_arrayNum = new int[5];
+            m_arrayDoorsValue = new int[5];
             m_levelCoeff = new Stack<int>();
             this.Reset();
         }
 
-        public void Show()      //output array of numbers
+        private void Show()      //output array of numbers
         {
             Console.WriteLine("--------------");
-            foreach (int num in m_arrayNum)
+            foreach (int num in m_arrayDoorsValue)
             {
                 Console.Write(num + " ");
             }
@@ -29,66 +55,74 @@ namespace DoorsAndLevels
             Console.WriteLine("--------------");
         }
 
-        public void CalcLevel(int coeff)
+        private void CalcLevel(int doorValue)
         {
-            if (!m_arrayNum.Contains(coeff))    //array doesnt contains coeff
+            if (doorValue < 0)
             {
-                throw new Exception("Number is not in list! Choose again.");
+                exitCode = true;
+                return;
+            }
+            if (!m_arrayDoorsValue.Contains(doorValue))    //array doesnt contains coeff
+            {
+                Console.WriteLine("Number is not in list!");
+                return;
             }
 
 
-            if (coeff == 0)
+            if (doorValue == 0)
             {
                 if (m_levelCoeff.Count == 0)  //stack is empty
                 {
-                    throw new Exception("It is first level! Choose again.");
+                    Console.WriteLine("It is first level!");
+                    return;
+
                 }
                 int divider = m_levelCoeff.Pop();
-                for (int i = 0; i < m_arrayNum.Count(); i++)
+                for (int i = 0; i < m_arrayDoorsValue.Count(); i++)
                 {
-                    m_arrayNum[i] /= divider; // return for previous level
+                    m_arrayDoorsValue[i] /= divider; // return for previous level
                 }
             }
             else
             {
 
-                for (int i = 0; i < m_arrayNum.Count(); i++)
+                for (int i = 0; i < m_arrayDoorsValue.Count(); i++)
                 {
 
                     try
                     {
-                        m_arrayNum[i] = checked(m_arrayNum[i] * coeff); // go to next level 
+                        m_arrayDoorsValue[i] = checked(m_arrayDoorsValue[i] * doorValue); // go to next level 
                     }
                     catch (OverflowException)
                     {
-                        // if some value in m_arrayNum > maxValueInt32
+                        // if some value in m_arrayDoorsValue > maxValueInt32
                         this.Reset();
                         m_levelCoeff.Clear();
-                        throw new Exception("Congratulations! You have reached the maximum value. Lets try again.");
+                        Console.WriteLine("Congratulations! You have reached the maximum value. Lets try again.");
+                        return;
                     }
                 }
-                m_levelCoeff.Push(coeff);
+                m_levelCoeff.Push(doorValue);
             }
         }
 
         private void Reset()
         {
             var random = new Random();
-            int sizeLst = 9;
-            List<int> lstWithNum = new List<int>(sizeLst);
-            for (int i = 0; i < sizeLst; i++) //Add to List values 1..9
-                lstWithNum.Add(i+1);
+            int maxValue = 9;  //
+            var listWithValues = new List<int>(Enumerable.Range(1, 9));
+
 
             for (int i = 0; i < 4; i++)
             {
                 //Get random value from List and Remove that value from List
-                int randNum = random.Next(1, sizeLst);
-                m_arrayNum[i] = lstWithNum[randNum - 1];
-                lstWithNum.RemoveAt(randNum - 1);
-                sizeLst--; //Size List decrement
+                int index = random.Next(1, maxValue);
+                m_arrayDoorsValue[i] = listWithValues[index - 1];
+                listWithValues.RemoveAt(index - 1);
+                maxValue--; //max index value decrease
             }
 
-            m_arrayNum[4] = 0;
+            m_arrayDoorsValue[4] = 0;
         }
 
     }
