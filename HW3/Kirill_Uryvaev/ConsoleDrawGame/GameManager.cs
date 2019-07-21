5c4b467100d2e4574c0ee48b66ca6d753d26cc62
spore9 <spore9@yandex.ru>
2019-07-21 16:21:46 +0300

    Initial commit

---
+++ HW3/Kirill_Uryvaev/ConsoleDrawGame/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDrawGame
+{
+    class GameManager
+    {
+        public GameManager()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ HW3/Kirill_Uryvaev/ConsoleDrawGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleDrawGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager game = new GameManager();
+            game.Run();
+        }
+    }
+}
