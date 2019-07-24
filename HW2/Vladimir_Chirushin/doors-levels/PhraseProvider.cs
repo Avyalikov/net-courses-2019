f32579a12535b4fc3a6096c20afd33a0422e4c8d
vladimirchr <vladimirchirushin@gmail.com>
2019-07-24 22:26:23 +0300

    Deleted unnecessary comments

--- HW2/Vladimir_Chirushin/doors-levels/PhraseProvider.cs
+++ HW2/Vladimir_Chirushin/doors-levels/PhraseProvider.cs
@@ -13,16 +13,6 @@ namespace doors_levels
         
         public PhraseProvider(IFileParser fileParser, String languageFilePath)
         {
-            /*gamePhrases.Add(Phrase.welcome, "Welcome to amazing doors levels game!");
-            gamePhrases.Add(Phrase.weHaveDoors, "We have numbers: ");
-            gamePhrases.Add(Phrase.weSelectNumber, "We select number");
-            gamePhrases.Add(Phrase.andGoNextLevel, " and go to next level: ");
-            gamePhrases.Add(Phrase.andGoPrevLevel, "and go to previous level: ");
-            gamePhrases.Add(Phrase.itsFirstLevel, "It's first level. Cant get higher.");
-            gamePhrases.Add(Phrase.youGetToFar, "You get too far! Droped to first level");
-            gamePhrases.Add(Phrase.doorDoesntExist, "Door doesn't exist");
-            gamePhrases.Add(Phrase.pleaseEnterANumber, "Please enter a number.");*/
-
             this.fileParser = fileParser;
             this.languageFilePath = languageFilePath;
         }
