d89328c700ffa95521868e055e9646810056c920
kouldred <kouldred@yandex.ru>
2019-07-24 18:33:59 +0300

    Implemented IDoorsNumbersGenerator

---
+++ HW2/Beliaev_Pavel/NumbersGame/NumbersGame/DoorsNumbersGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NumbersGame.Interfaces;
+
+namespace NumbersGame
+{
+    public class DoorsNumbersGenerator : IDoorsNumbersGenerator //TODO
+    {
+        public int[] GenerateDoorsNumbers(int doorsAmount)
+        {
+            int[] doorsNumbers = new int[doorsAmount];
+            doorsNumbers[0] = 0;
+            Random rnd = new Random();
+            for(int door = 1; door < doorsAmount; door++)
+            {
+                door = rnd.Next(1, 9);
+            }
+            return doorsNumbers;
+        }
+    }
+}
