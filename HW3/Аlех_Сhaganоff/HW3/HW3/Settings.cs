a39c7dc5fc197041feb72af6184b7cd4c84c7cad
alch-0 <52721901+alch-0@users.noreply.github.com>
2019-07-24 17:06:46 +0300

    HW3

---
+++ HW3/Аlех_Сhaganоff/HW3/HW3/Settings.cs
@@ -0,0 +1,47 @@
+﻿namespace HW3
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    public class Settings
+    {
+        public string KeyDrawDot  { get; set; }
+        public string KeyDrawHorizontalLine { get; set; }
+        public string KeyDrawVerticalLine { get; set; }
+        public string KeyDrawSnowFlake { get; set; }
+        public string KeyClear { get; set; }
+        public string KeyExit { get; set; }
+
+        public int BoardSizeX { get; set; }
+        public int BoardSizeY { get; set; }
+
+        [XmlIgnore]
+        public Dictionary<string, string> AllMenuKeys { get; set; }
+
+        public Settings()
+        {
+            KeyDrawDot = "1";
+            KeyDrawHorizontalLine = "2";
+            KeyDrawVerticalLine = "3";
+            KeyDrawSnowFlake = "4";
+            KeyClear = "5";
+            KeyExit = "0";
+            
+            BoardSizeX = 32;
+            BoardSizeY = 16;
+        }
+
+        public void InitializeAllMenuKeys()
+        {
+            AllMenuKeys = new Dictionary<string, string>()
+            {
+                { KeyDrawDot, nameof(KeyDrawDot) },
+                { KeyDrawHorizontalLine, nameof(KeyDrawHorizontalLine) },
+                { KeyDrawVerticalLine,nameof(KeyDrawVerticalLine) },
+                { KeyDrawSnowFlake,nameof(KeyDrawSnowFlake) },
+                { KeyClear,nameof(KeyClear) },
+                { KeyExit,nameof(KeyExit) }
+            };
+        }
+    }      
+}
---
+++ HW3/Аlех_Сhaganоff/HW3/HW3/XMLSettings.cs
@@ -0,0 +1,39 @@
+﻿namespace HW3
+{
+    using System.IO;
+    using System.Xml.Serialization;
+    using System;
+
+    public class XMLSettings : ISettingsProvider
+    {
+        string path;
+        public Settings GetSettings()
+        {
+            Stream stream = null;
+            XmlSerializer xmlSerazlizer;
+            Settings settings;
+
+            try
+            {
+                stream = new FileStream(path, FileMode.Open);
+                xmlSerazlizer = new XmlSerializer(typeof(Settings));
+                settings = (Settings)xmlSerazlizer.Deserialize(stream);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+            return settings;
+        }
+
+        public XMLSettings(string path)
+        {
+            this.path = path;
+        }
+    }
+}
