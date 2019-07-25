4af2e5f7e8ec660a9d432498a2c6aee206788b12
Avyalikov <avyalikov@mail.ru>
2019-07-25 04:17:17 +0300

    Extracted doors generator component and wrote an interface for it.

---
+++ HW2/Vyalikov_Anton/Doors-and-levels-0.2/DoorsAndLevels/DoorsNumGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoorsAndLevels
+{
+    class DoorsNumGenerator : Interfaces.IDoorsGenerator
+    {
+        public List<int> GetDoorsNumbers(int doorsCount)
+        {
+            List<int> doorsNumbers = new List<int>();
+            Random genNum = new Random();
+            doorsNumbers.Add(0);
+
+            for (int i = 0; i < doorsCount - 1; i++)
+            {
+                while (true)
+                {
+
+                    int number = genNum.Next(2, 9);
+                    if (!doorsNumbers.Contains(number))
+                    {
+                        doorsNumbers.Add(number);
+                        break;
+                    }
+                }
+            }
+
+            return doorsNumbers;
+        }
+    }
+}
