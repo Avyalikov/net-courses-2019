e5ca80aceed01192a3b9d43089951fb37efe1eac
Евгений Никулин Алексеевич <gn.nage@gmail.com>
2019-07-28 12:01:58 +0300

    Created the draw simulator

--- HW2/Evgeniy_Nikulin/Doors_and_levels_game/Doors_and_levels_game/Game.cs
+++ HW2/Evgeniy_Nikulin/Doors_and_levels_game/Doors_and_levels_game/Game.cs
@@ -1,107 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using Doors_and_levels_game.Interfaces;
+﻿    using System.Collections.Generic;
+    using System.IO;
+    using System;
+    using Interfaces;
+    using Newtonsoft.Json;
 
-namespace Doors_and_levels_game
-{
-    public class Game
-    {
-        private readonly GameSettings settings;
-        private readonly IPhraseProvider phraseProvider;
-        private readonly InputOutputModule io;
-        private readonly IDoorsGenerater<List<ulong>> doorsGenerater;
-        private readonly IStorageModule<ulong, List<ulong>> st;
-        public Game(
-            GameSettings settings,
-            IPhraseProvider phraseProvider,
-            InputOutputModule io,
-            IDoorsGenerater<List<ulong>> doorsGenerater,
-            IStorageModule<ulong, List<ulong>> storage
+    namespace Doors_and_levels_game {
+        public class Game {
+            private readonly GameSettings settings;
+            private readonly IPhraseProvider phraseProvider;
+            private readonly InputOutputModule io;
+            private readonly IDoorsGenerater<List<ulong>> doorsGenerater;
+            private readonly IStorageModule<ulong, List<ulong>> st;
+            public Game(
+                GameSettings settings,
+                IPhraseProvider phraseProvider,
+                InputOutputModule io,
+                IDoorsGenerater<List<ulong>> doorsGenerater,
+                IStorageModule<ulong, List<ulong>> storage
             ) {
-            this.settings = settings;
-            this.phraseProvider = phraseProvider;
-            this.io = io;
-            this.doorsGenerater = doorsGenerater;
-            this.st = storage;
-        }
+                this.settings = settings;
+                this.phraseProvider = phraseProvider;
+                this.io = io;
+                this.doorsGenerater = doorsGenerater;
+                this.st = storage;
+            }
 
-        public void Start()
-        {
-            // Initiation random doors
-            st.Doors = doorsGenerater.Generate(settings.doorsAmount);
-            
-            // Start
-            io.Print(phraseProvider.GetPhrase(Phrase.Welcome));
-            io.Print(st.Doors);
+            public void Start() {
+                // Initiation random doors
+                st.Doors = doorsGenerater.Generate(settings.doorsAmount);
 
-            // Main loop
-            ulong door;
-            int lvl = 1;
-            while (true)
-            {
-                door = GetDoor();
-                io.Print();
-                if (door == 0)
-                {
-                    io.Print(phraseProvider.GetPhrase(Phrase.MoveBack));
-                    PreviousLvl();
-                    if(lvl > 1) lvl--;
-                } else {
-                    io.Print(phraseProvider.GetPhrase(Phrase.MoveForward));
-                    NextLvl(door);
-                    lvl++;
-                }
-
-                if (lvl > settings.maxLvL)
-                {
-                    io.Print(phraseProvider.GetPhrase(Phrase.YouWin));
-                    return;
-                }
-
+                // Start
+                io.Print(phraseProvider.GetPhrase(Phrase.Welcome));
                 io.Print(st.Doors);
-            }
-        }     
 
-        ulong GetDoor()
-        {
-            ulong num;
-            string str;
-
-            while (true)
-            {
-                io.Print(phraseProvider.GetPhrase(Phrase.YourChoose), end: "");
-                str = io.Input();
-                try
-                {
-                    num = Convert.ToUInt64(str);
-                    if (st.Doors.Contains(num))
-                    {
-                        return num;
+                // Main loop
+                ulong door;
+                int lvl = 1;
+                while (true) {
+                    door = GetDoor();
+                    io.Print();
+                    if (door == 0) {
+                        io.Print(phraseProvider.GetPhrase(Phrase.MoveBack));
+                        PreviousLvl();
+                        if (lvl > 1) lvl--;
+                    } else {
+                        io.Print(phraseProvider.GetPhrase(Phrase.MoveForward));
+                        NextLvl(door);
+                        lvl++;
                     }
-                } catch { }
 
-                io.Print(phraseProvider.GetPhrase(Phrase.WrongValue));
-            }
-        } 
+                    if (lvl > settings.maxLvL) {
+                        io.Print(phraseProvider.GetPhrase(Phrase.YouWin));
+                        return;
+                    }
 
-        void NextLvl(ulong door)
-        {
-            st.PushChosenDoor(door);
-            for (int i = 0; i < st.Doors.Count; i++)
-            {
-                st.Doors[i] *= door;
+                    io.Print(st.Doors);
+                }
             }
-        }
-        void PreviousLvl()
-        {
-            if (!st.ChosenDoorIsEmpty())
-            {
-                ulong door = st.PopChosenDoor();
-                for (int i = 0; i < st.Doors.Count; i++)
-                {
-                    st.Doors[i] /= door;
+
+            ulong GetDoor() {
+                ulong num;
+                string str;
+
+                while (true) {
+                    io.Print(phraseProvider.GetPhrase(Phrase.YourChoose), end: "");
+                    str = io.Input();
+                    try {
+                        num = Convert.ToUInt64(str);
+                        if (st.Doors.Contains(num)) {
+                            return num;
+                        }
+                    } catch { }
+
+                    io.Print(phraseProvider.GetPhrase(Phrase.WrongValue));
+                }
+            }
+
+            void NextLvl(ulong door) {
+                st.PushChosenDoor(door);
+                for (int i = 0; i < st.Doors.Count; i++) {
+                    st.Doors[i] *= door;
+                }
+            }
+            void PreviousLvl() {
+                if (!st.ChosenDoorIsEmpty()) {
+                    ulong door = st.PopChosenDoor();
+                    for (int i = 0; i < st.Doors.Count; i++) {
+                        st.Doors[i] /= door;
+                    }
                 }
             }
         }
     }
-}
---
+++ HW3/Evgeniy_Nikulin/The draw game/Program.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="EPAM">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace The_draw_game
+{
+    using Components;
+    using Interfaces;
+
+    /// <summary>
+    /// Entering point class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entering method
+        /// </summary>
+        public static void Main()
+        {
+            ISettingsProvider settingsProvider = new SettingsProvider();
+            var settings = settingsProvider.Get();
+
+            IInputOutput inputOutput = new ConsoleIOModule();
+
+            IBoard board = new Board(inputOutput, settings.BorderWidth, settings.BorderHeight, settings.BorserStile);
+
+            IPhraseProvider phraseProvider = new JsonPhraseProvider();
+            phraseProvider.Init(settings.Language);
+
+            IFigureManager figureManager = new FigureManager(inputOutput, settings.FigureStile);
+
+            Game game = new Game(
+                st: settings, 
+                board: board,
+                io: inputOutput,
+                phraseProvider: phraseProvider,
+                figureManager: figureManager);
+
+            game.Run();
+        }
+    }
+}
