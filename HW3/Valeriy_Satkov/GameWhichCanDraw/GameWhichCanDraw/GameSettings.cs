c0fce4bc7ea954d3c0311c6dfba4b581726f02fc
Valeriy Satkov <valeriy.satkov@gmail.com>
2019-07-30 17:25:13 +0300

    Add check for wrong size of board in json settings file

--- HW3/Valeriy_Satkov/GameWhichCanDraw/GameWhichCanDraw/GameSettings.cs
+++ HW3/Valeriy_Satkov/GameWhichCanDraw/GameWhichCanDraw/GameSettings.cs
@@ -5,20 +5,64 @@
 
 namespace GameWhichCanDraw
 {
+    using System;
+
     /// <summary>
     /// Game settings class
     /// </summary>
     public class GameSettings
     {
         /// <summary>
+        /// the Length of board
+        /// </summary>
+        private int length;
+
+        /// <summary>
+        /// the Length of board
+        /// </summary>
+        private int width;
+
+        /// <summary>
         /// Gets or sets the Length of board
         /// </summary>
-        public int Length { get; set; }
+        public int Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            set
+            {
+                if (value < 7)
+                {
+                    throw new ArgumentException($"Incorrect Length = {value} in settings file. It should not be less than 7");
+                }
+
+                this.length = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the Width of board
         /// </summary>
-        public int Width { get; set; }
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 7)
+                {
+                    throw new ArgumentException($"Incorrect Width = {value} in settings file. It should not be less than 7");
+                }
+
+                this.width = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the String for exit
