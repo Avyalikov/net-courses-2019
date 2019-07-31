e5f3821ac5794a7f45bdfdb5d0f31c9483ce97ad
Valeriy Satkov <valeriy.satkov@gmail.com>
2019-07-31 23:02:14 +0300

    Add solutions 1.1.1, 1.1.2

--- HW4/Valeriy_Satkov/1.1.1.sql
+++ HW4/Valeriy_Satkov/1.1.1.sql
@@ -1,1 +1,4 @@
-﻿-- Start
+﻿USE Northwind;
+DECLARE @datetime2 datetime2 = '1996-07-16';
+SELECT OrderID, ShippedDate, ShipVia FROM Orders
+WHERE ShippedDate >= @datetime2 AND ShipVia >= 2;
---
+++ HW4/Valeriy_Satkov/1.1.2.sql
@@ -0,0 +1,7 @@
+﻿USE Northwind;
+SELECT OrderID, ShippedDate =
+      CASE
+	     WHEN ShippedDate IS NULL THEN 'Not Shipped'  
+      END
+FROM Orders
+WHERE ShippedDate IS NULL;
