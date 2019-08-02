e58fe3502c22eccb377edf8b200beaf6d280e4a7
vladimirchr <vladimirchirushin@gmail.com>
2019-08-03 00:49:08 +0300

    Created SQL project and added First pard of Stored Procedures

--- HW4/Vladimir_Chirushin/Task_1_4.sql
+++
@@ -1,4 +0,0 @@
--- Task 1.4 #1
-SELECT ProductName
-FROM dbo.Products
-WHERE ProductName LIKE '%cho_olade%'
---
+++ HW4/Vladimir_Chirushin/sql-queries/dbo/Stored Procedures/111OrdersFilteredByDate.sql
@@ -0,0 +1,12 @@
+﻿-- Task 1.1 #1
+CREATE PROCEDURE OrdersFilteredByDate
+AS
+DECLARE @ShippedDateLimit datetime = '06-may-1998'
+SELECT 
+	OrderID, 
+	ShippedDate, 
+	ShipVia
+FROM dbo.Orders
+WHERE 
+	ShippedDate >= CAST(@ShippedDateLimit as date) AND 
+	ShipVia >= 2
