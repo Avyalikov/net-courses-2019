9e9fa38c8baf921ddd7e7946442bb225b69ab0fd
Vladimir Chirushin <52735362+vladimirchr@users.noreply.github.com>
2019-08-03 22:05:35 +0300

    Update 225FindCustomersInCity.sql

--- HW4/Vladimir_Chirushin/sql-queries/dbo/Stored Procedures/225FindCustomersInCity.sql
+++ HW4/Vladimir_Chirushin/sql-queries/dbo/Stored Procedures/225FindCustomersInCity.sql
@@ -1,13 +1,17 @@
 ﻿-- Task 2.2 #5
 CREATE PROCEDURE FindCustomersInCity
 AS
-SELECT  
-	DISTINCT Customers.City, 
-	Customers.ContactName AS 'Customer'
+SELECT 
+	Customers.ContactName
 FROM 
-	dbo.Customers 
-GROUP BY 
-	Customers.ContactName, 
-	Customers.City
-ORDER BY 
-	Customers.City
+	dbo.Customers
+WHERE Customers.City IN 
+		(SELECT 
+			Customers.City
+		FROM 
+			dbo.Customers
+		GROUP BY 
+			Customers.City
+		HAVING 
+			COUNT(Customers.CustomerID) >= 2)
+ORDER BY Customers.ContactName
