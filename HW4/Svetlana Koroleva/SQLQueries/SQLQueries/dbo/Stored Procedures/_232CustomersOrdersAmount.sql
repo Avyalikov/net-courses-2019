db6d402c0334e082f8dec9a78eaba3a8313f7ee9
korolsv <koroleva-sa@outlook.com>
2019-08-03 08:47:48 +0300

    Created Stored Procedures for SQL Queries

---
+++ HW4/Svetlana Koroleva/SQLQueries/SQLQueries/dbo/Stored Procedures/_232CustomersOrdersAmount.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[_232CustomersOrdersAmount]
+AS
+	SELECT c.ContactName, COUNT(od.Quantity) as Amount
+    FROM Customers as c LEFT JOIN Orders as o
+    ON c.CustomerID=o.CustomerID
+    JOIN [Order Details] as od
+    ON o.OrderID=od.OrderID
+    GROUP BY ContactName
