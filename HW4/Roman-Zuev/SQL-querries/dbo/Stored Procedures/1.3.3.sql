e931c3420dac9e5aea03a632faf44cbeea21afe5
hobowithshotgun <strochwow12@gmail.com>
2019-08-03 21:03:42 +0300

    Queries added

---
+++ HW4/Roman-Zuev/SQL-querries/dbo/Stored Procedures/1.3.3.sql
@@ -0,0 +1,10 @@
+﻿--Выбрать всех заказчиков из таблицы Customers, у которых название 
+--страны начинается на буквы из диапазона b и g, не используя оператор BETWEEN.
+
+CREATE PROCEDURE [1.3.3]
+AS
+
+SELECT CustomerID, Country
+FROM Customers
+WHERE Country >'b' AND Country <'h'
+ORDER BY Country
---
+++ HW4/Roman-Zuev/SQL-querries/dbo/Stored Procedures/2.2.1.sql
@@ -0,0 +1,10 @@
+﻿--По таблице Orders найти количество заказов с группировкой по годам.
+--В результатах запроса надо возвращать две колонки c названиями Year и Total.
+--Написать проверочный запрос, который вычисляет количество всех заказов.
+
+CREATE PROCEDURE [2.2.1]
+AS
+
+SELECT year(OrderDate) AS 'Year', COUNT (OrderID) AS 'Total'
+FROM Orders
+GROUP BY year(OrderDate)
