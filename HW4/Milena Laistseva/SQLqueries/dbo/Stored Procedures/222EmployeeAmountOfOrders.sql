74b152ea481cb6f333e8d1decc33b761f9cf3e31
MilRoad <milena_laistseva@mail.ru>
2019-08-04 15:00:30 +0300

    Added copy of Northwind database and sql queries from the tasks

---
+++ HW4/Milena Laistseva/SQLqueries/dbo/Stored Procedures/222EmployeeAmountOfOrders.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[222EmployeeAmountOfOrders]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT 
+      (SELECT CONCAT (LastName, ' ', FirstName) 
+         FROM Employees 
+         WHERE EmployeeID = Orders.EmployeeID) AS Seller,
+       COUNT(EmployeeID) as Amount
+FROM Orders
+GROUP BY EmployeeID
+ORDER BY Amount DESC
---
+++ HW4/Milena Laistseva/SQLqueries/dbo/Stored Procedures/224CustomerAndEmployeeSameCity.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[224CustomerAndEmployeeSameCity]
+AS
+	SELECT
+CONCAT (Employees.LastName, ' ', Employees.FirstName) AS Seller,
+Customers.ContactName AS Customer
+FROM Employees, Customers
+WHERE Employees.City = Customers.City
