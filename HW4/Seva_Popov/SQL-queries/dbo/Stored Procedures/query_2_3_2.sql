09352dc1f1990dd70b6f3f64006c1c405a17f104
sevap8 <34450118+sevap8@users.noreply.github.com>
2019-08-04 23:56:06 +0300

    Update query_2_3_2.sql

--- HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_3_2.sql
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_3_2.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [query_2_3_2]
 AS	
-	SELECT ContactName, COUNT(OrderID) AS Amount
+	SELECT ContactName, COUNT(OrderID) as Amount
 	FROM Customers 
 	LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
 	GROUP BY ContactName
