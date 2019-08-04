9db0253b99923fd42c8f2ab034e5e874e3728973
sevap8 <34450118+sevap8@users.noreply.github.com>
2019-08-04 23:55:20 +0300

    Update query_2_2_3.sql

--- HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_2_3.sql
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_2_3.sql
@@ -1,13 +1,13 @@
 ﻿CREATE PROCEDURE [query_2_2_3]
 AS	
 	SELECT
-		(SELECT (LastName + ' ' + FirstName)
-		FROM Employees emp 
-		WHERE emp.EmployeeID = Orders.EmployeeID) AS 'Seller',
-		(SELECT ContactName 
-		FROM Customers 
-		WHERE Customers.CustomerID = Orders.CustomerID) AS 'Customer',
-		COUNT(OrderID) AS 'Amount'
+	(SELECT (LastName + ' ' + FirstName)
+	FROM Employees emp 
+	WHERE emp.EmployeeID = Orders.EmployeeID) as 'Seller',
+	(SELECT ContactName 
+	FROM Customers 
+	WHERE Customers.CustomerID = Orders.CustomerID) as 'Customer',
+	COUNT(OrderID) as 'Amount'
 	FROM Orders
 	WHERE YEAR(OrderDate) = 1998
 	GROUP BY Orders.CustomerID, Orders.EmployeeID
