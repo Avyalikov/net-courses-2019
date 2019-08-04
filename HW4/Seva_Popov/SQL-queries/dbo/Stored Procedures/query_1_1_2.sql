9ac73ad6bc0222b4908065b92797b7699c98b898
sevap <sevap@DESKTOP-PS4FLU8>
2019-08-04 18:41:16 +0300

    add Sql queries

---
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_1_1_2.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[query_1_1_2]
+AS
+
+	SELECT OrderID,
+	CASE
+	WHEN ShippedDate IS NULL
+	THEN 'Not Shipped'
+	END ShippedDate
+	FROM Orders
+	WHERE ShippedDate IS NULL
---
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_1_2_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [query_1_2_1]
+AS
+
+	SELECT ContactName, Country
+	FROM Customers
+	WHERE Country IN ('USA', 'Canada')
+	ORDER BY ContactName, Address
