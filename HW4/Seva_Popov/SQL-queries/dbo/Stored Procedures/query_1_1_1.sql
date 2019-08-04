7e7b6d63188a769a2c14f0f206dcc9f09b2ef599
sevap8 <34450118+sevap8@users.noreply.github.com>
2019-08-04 18:42:55 +0300

    Update query_1_1_1.sql

--- HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_1_1_1.sql
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_1_1_1.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[query_1_1_1]
 AS
 
-SELECT OrderID as 'Order', ShippedDate as 'Shipped Date', ShipVia as 'Ship via'
+	SELECT OrderID as 'Order', ShippedDate as 'Shipped Date', ShipVia as 'Ship via'
 	FROM Orders
 	WHERE ShipVia >= 2 AND ShippedDate >= '1998-05-06'
