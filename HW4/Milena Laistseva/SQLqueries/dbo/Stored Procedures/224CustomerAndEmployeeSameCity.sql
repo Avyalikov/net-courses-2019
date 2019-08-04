2887b94b50664752895c0f8ba13138479ba1a43d
MilRoad <milena_laistseva@mail.ru>
2019-08-04 21:38:44 +0300

    Changed 224 store procedure, added UNION operator

--- HW4/Milena Laistseva/SQLqueries/dbo/Stored Procedures/224CustomerAndEmployeeSameCity.sql
+++ HW4/Milena Laistseva/SQLqueries/dbo/Stored Procedures/224CustomerAndEmployeeSameCity.sql
@@ -1,7 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[224CustomerAndEmployeeSameCity]
 AS
-	SELECT
-CONCAT (Employees.LastName, ' ', Employees.FirstName) AS Seller,
-Customers.ContactName AS Customer
+	SELECT Customers.ContactName AS Customer
+FROM Employees, Customers
+WHERE Employees.City = Customers.City
+UNION
+SELECT CONCAT (Employees.LastName, ' ', Employees.FirstName) AS Seller
 FROM Employees, Customers
 WHERE Employees.City = Customers.City
