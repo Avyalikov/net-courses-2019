1d6e41de9dd757173dfbae2a0eecb05639cf8767
sevap8 <34450118+sevap8@users.noreply.github.com>
2019-08-04 23:56:19 +0300

    Update query_2_4_1.sql

--- HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_4_1.sql
+++ HW4/Seva_Popov/SQL-queries/dbo/Stored Procedures/query_2_4_1.sql
@@ -3,6 +3,6 @@ AS
 	SELECT CompanyName
 	FROM Suppliers
 	WHERE SupplierID IN 
-		(SELECT SupplierID
-		FROM Products
-		WHERE UnitsInStock = 0)
+	(SELECT SupplierID
+	FROM Products
+	WHERE UnitsInStock = 0)
