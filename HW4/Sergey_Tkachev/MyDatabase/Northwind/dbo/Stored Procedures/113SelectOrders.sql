ae2794efb3afbe678684e70f2e63c16aa483f7e8
Foxtra <tkachew.sergey@ya.ru>
2019-08-04 23:50:37 +0300

    fix 1.1.1 fix 1.1.3

---
+++ HW4/Sergey_Tkachev/MyDatabase/Northwind/dbo/Stored Procedures/113SelectOrders.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE PROCEDURE [dbo].[113SelectOrders](
+	@shipDate DATETIME)
+AS 
+BEGIN
+
+SELECT [OrderID] as "Order Number", CASE
+WHEN [ShippedDate] is NULL THEN 'Not Shipped' ELSE CAST([ShippedDate] AS nvarchar)
+END AS [ShippedDate]
+FROM [dbo].[Orders]
+WHERE [ShippedDate] >= @shipDate OR [ShippedDate] Is NULL
+
+END
