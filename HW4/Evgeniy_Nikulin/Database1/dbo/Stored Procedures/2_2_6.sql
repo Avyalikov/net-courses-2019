3d13a60589eb3745b9f078d279a2736cb5cacf92
Nage <gn.nage@gmail.com>
2019-08-04 16:23:37 +0300

    Cheate sql queries

---
+++ HW4/Evgeniy_Nikulin/Database1/dbo/Stored Procedures/2_2_6.sql
@@ -0,0 +1,10 @@
+﻿create procedure [dbo].[2_2_6]
+as
+	select 
+		'Employee' = [LastName] + ' ' + [FirstName],
+		'Menagers' = (
+			select [Menagers].[LastName] + ' ' + [Menagers].[FirstName]
+			from [Employees] as [Menagers]
+			where [Menagers].[EmployeeID] = [Employees].[ReportsTo])
+	from [Employees]
+return 0
