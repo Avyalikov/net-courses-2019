1f0dbc589ef82da4f0a45217a2afcf1dabb97479
spore9 <spore9@yandex.ru>
2019-08-07 22:11:57 +0300

    Partly solved task

--- HW5/Kirill_Uryvaev/Task/LinqSamples.cs
+++ HW5/Kirill_Uryvaev/Task/LinqSamples.cs
@@ -61,5 +61,134 @@ namespace SampleQueries
 			}
 		}
 
-	}
+        [Category("Home Task XML")]
+        [Title("Linq001")]
+        [Description("This sample return all customers that have order ")]
+
+        public void Linq001()
+        {
+            int[] requeredSum = { 10000, 5000, 15000 };
+            foreach (int r in requeredSum)
+            {
+                var clients = dataSource.Customers.Where(x => x.Orders.Sum(y => y.Total) > r).
+                    Select(x=>new { x.CustomerID, x.CompanyName, OrdersSum = x.Orders.Sum(y => y.Total) });
+                ObjectDumper.Write($"Requested sum {r}:");
+                foreach (var c in clients)
+                {
+                    ObjectDumper.Write(c);
+                }
+            }
+        }
+
+        [Category("Home Task XML")]
+        [Title("Linq002")]
+        [Description("This sample return all customers and suppliers in same city and country ")]
+
+        public void Linq002()
+        {
+            var clients =
+                from c in dataSource.Customers
+                from s in dataSource.Suppliers
+                where c.Country == s.Country && c.City==s.City
+                select new { c.City, c.Country, c.CompanyName, s.SupplierName};
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+
+        /*[Category("Home Task XML")]
+        [Title("Linq002x")]
+        [Description("This sample return all customers and suppliers in same city and country with group by")]
+
+        public void Linq002x()
+        {
+            var clients =
+                from c in dataSource.Customers
+                from s in dataSource.Suppliers
+                group new {c,s} by new { s.City, s.Country } into g
+                select g;
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }*/
+
+        [Category("Home Task XML")]
+        [Title("Linq003")]
+        [Description("This sample return all customers that have order bigger than r ")]
+
+        public void Linq003()
+        {
+            int r = 1000;
+            var clients = dataSource.Customers.Where(x => x.Orders.Any(y => y.Total>r)).
+                Select(x=>new { x.CompanyName, MaxValue = x.Orders.Max(y=>y.Total)});
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+
+        [Category("Home Task XML")]
+        [Title("Linq004")]
+        [Description("This sample return all customers with orders and date of their first order ")]
+
+        public void Linq004()
+        {
+            var clients = dataSource.Customers.Where(x=>x.Orders.Count()>0).
+                Select(x => new { x.CompanyName, ClientFrom = x.Orders.Min(y => y.OrderDate) });
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+
+        [Category("Home Task XML")]
+        [Title("Linq005")]
+        [Description("This sample return all customers with orders and date of their first order and ordered by")]
+
+        public void Linq005()
+        {
+            var clients = dataSource.Customers.Where(x => x.Orders.Count() > 0).
+                Select(x => new { x.CompanyName, ClientFrom = x.Orders.Min(y => y.OrderDate), OrdersSum = x.Orders.Sum(y=>y.Total) }).
+                OrderBy(x => x.CompanyName).OrderByDescending(x => x.OrdersSum).OrderBy(x => x.ClientFrom.Month).OrderBy(x=>x.ClientFrom.Year);
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+
+        [Category("Home Task XML")]
+        [Title("Linq006")]
+        [Description("This sample return all customers with specific properties")]
+
+        public void Linq006()
+        {
+            var clients = dataSource.Customers.Where(x => x.Region == null || x.Phone[0]!='(' || !x.PostalCode.All(char.IsDigit));
+
+            foreach (var c in clients)
+            {
+                ObjectDumper.Write(c);
+            }
+        }
+
+        /*[Category("Home Task XML")]
+        [Title("Linq007")]
+        [Description("This sample return categorized products")]
+
+        public void Linq007()
+        {
+            var products = dataSource.Products.GroupBy(x=>x.Category).GroupBy(x=>x);
+
+            foreach (var p in products)
+            {
+                ObjectDumper.Write(p);
+            }
+        }*/
+    }
 }
