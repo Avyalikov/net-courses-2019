0fbf542d04ea0ecb0e7660feffc568b6dc28bfaf
vladimirchr <vladimirchirushin@gmail.com>
2019-08-09 16:38:30 +0300

    Minor code clearence

--- HW5/LinqSamples/Task/Task/LinqSamples.cs
+++ HW5/LinqSamples/Task/Task/LinqSamples.cs
@@ -1,45 +1,33 @@
-﻿// Copyright © Microsoft Corporation.  All Rights Reserved.
-// This code released under the terms of the 
-// Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.)
-//
-//Copyright (C) Microsoft Corporation.  All rights reserved.
-
-using SampleSupport;
-using System.Collections.Generic;
-using System.Linq;
-using Task.Data;
-
-// Version Mad01
-
-namespace SampleQueries
+﻿namespace SampleQueries
 {
+    using System.Linq;
+    using SampleSupport;
+    using Task.Data;
+
     [Title("LINQ Module")]
     [Prefix("Linq")]
     public class LinqSamples : SampleHarness
     {
         private DataSource dataSource = new DataSource();
 
-        public class Customers
-        {
-            public string CompanyName;
-            public decimal Total;
-        }
         [Category("Basic LINQ Operations")]
         [Title("TaskA01-Customers w/total")]
         [Description("Get all customers that spend more than X on products")]
         public void LinqA01()
         {
-            IEnumerable<Customers> customers =
-               from cust in dataSource.Customers
+            var customers =
+               from cust in this.dataSource.Customers
                orderby cust.Orders.Sum(o => o.Total) descending
-               select new Customers { CompanyName = cust.CompanyName, Total = cust.Orders.Sum(o => o.Total) };
+               select new { CompanyName = cust.CompanyName, Total = cust.Orders.Sum(o => o.Total) };
 
             void ShowCustomerIfTotal(decimal total)
             {
                 foreach (var customer in customers)
                 {
                     if (customer.Total > total)
+                    {
                         ObjectDumper.Write(customer);
+                    }
                 }
             }
 
@@ -57,30 +45,40 @@ namespace SampleQueries
         public void LinqA02()
         {
             var customers =
-                from cust in dataSource.Customers
-                from supp in dataSource.Suppliers
-                where cust.City == supp.City
-                select new { cust.CompanyName, supp.SupplierName };
+                from cust in this.dataSource.Customers
+                join supp in this.dataSource.Suppliers
+                on cust.City equals supp.City  into custSupp
+                select new { cust.CompanyName, custSupp };
 
             foreach (var customer in customers)
             {
-                ObjectDumper.Write(customer);
+                ObjectDumper.Write(customer,2);
             }
 
-            //second variant with join
-            ObjectDumper.Write("________________");
-            var customers2 =
-               from cust in dataSource.Customers
-               join supp in dataSource.Suppliers on cust.City equals supp.City into custSupGroup
-               select new
-               {
-                   City = cust.City,
-                   Supplier = from supGroup in custSupGroup
-                              select new { SupplierName = supGroup.SupplierName },
-                   Customer = cust.CompanyName
-               };
+            // second variant with group
+            ObjectDumper.Write("*****************************");
+            var customer2 = 
+                from cust in this.dataSource.Customers
+                group cust by cust.City into cityGroup
+                select new
+                {
+                    City = cityGroup.Key,
+                    Customer =
+                        from cust in cityGroup
+                        select new
+                        {
+                            Name = cust.CompanyName,
+                            Supplier =
+                                from supp in this.dataSource.Suppliers
+                                where supp.City == cityGroup.Key
+                                    select new
+                                    {
+                                        supp.SupplierName
+                                    }
+                        }
+                };
 
-            foreach (var customer in customers2)
+            foreach (var customer in customer2)
             {
                 ObjectDumper.Write(customer, 2);
             }
@@ -93,7 +91,7 @@ namespace SampleQueries
         {
             decimal minOrderValue = 8000;
             var customers =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 let maxOrder = cust.Orders.Any() ? cust.Orders.Max(o => o.Total) : 0
                 select new { cust.CompanyName, maxOrder };
 
@@ -101,7 +99,9 @@ namespace SampleQueries
             foreach (var customer in customers)
             {
                 if (customer.maxOrder > minOrderValue)
+                {
                     ObjectDumper.Write(customer);
+                }
             }
         }
 
@@ -111,7 +111,7 @@ namespace SampleQueries
         public void LinqA04()
         {
             var customers =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 where cust.Orders.Any()
                 select new
                 {
@@ -127,14 +127,13 @@ namespace SampleQueries
             }
         }
 
-
         [Category("Basic LINQ Operations")]
         [Title("TaskA05-CustomersFirstOrderAndTotal")]
         [Description("Customer first order and total sum of orders")]
         public void LinqA05()
         {
             var customers =
-                   from cust in dataSource.Customers
+                   from cust in this.dataSource.Customers
                    where cust.Orders.Any()
                    orderby
                         cust.Orders.ElementAt(0).OrderDate.Year,
@@ -162,7 +161,7 @@ namespace SampleQueries
         {
             int tryInteger;
             var customers =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 where ((cust.Region == null) || (!int.TryParse(cust.PostalCode, out tryInteger)) || (cust.Phone[0] != '('))
                 select new { cust.CompanyName, cust.Region, cust.PostalCode, cust.Phone };
 
@@ -178,7 +177,7 @@ namespace SampleQueries
         public void LinqA07()
         {
             var products =
-                from prod in dataSource.Products
+                from prod in this.dataSource.Products
                 group prod by prod.Category into categoryGroup
                 select new
                 {
@@ -204,16 +203,18 @@ namespace SampleQueries
 
         [Category("Basic LINQ Operations")]
         [Title("TaskA08-GroupProductByPrice")]
-        [Description("Group orders in cheap and expensive categorys")]
+        [Description("Group orders in cheap, expensive and middle price categories")]
         public void LinqA08()
         {
             decimal cheapThreshold = 20;
             decimal expensiveThreshold = 40;
 
             var products =
-                from prod in dataSource.Products
-                group prod by (prod.UnitPrice < cheapThreshold ? "Cheap" :
-                                prod.UnitPrice > expensiveThreshold ? "Expensive" : "Middle price") into priceGroup
+                from prod in this.dataSource.Products
+                group prod by (
+                                prod.UnitPrice < cheapThreshold ? "Cheap" :
+                                    prod.UnitPrice > expensiveThreshold ? "Expensive" : "Middle price"
+                                ) into priceGroup
                 select new { priceGroup.Key, priceGroup };
 
             foreach (var prod in products)
@@ -228,7 +229,7 @@ namespace SampleQueries
         public void LinqA09()
         {
             var averageProfit =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 group cust by cust.City into cityGroup
                 select new
                 {
@@ -249,7 +250,7 @@ namespace SampleQueries
         public void LinqA10()
         {
             var customersOrdersByMonth =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 from ordrs in cust.Orders
                 group ordrs by ordrs.OrderDate.Month into monthGroup
                 orderby monthGroup.Key
@@ -266,7 +267,7 @@ namespace SampleQueries
 
             ObjectDumper.Write("_____________________________");
             var customersOrdersByYear =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 from ordrs in cust.Orders
                 group ordrs by ordrs.OrderDate.Year into yearGroup
                 orderby yearGroup.Key
@@ -283,7 +284,7 @@ namespace SampleQueries
 
             ObjectDumper.Write("_____________________________");
             var customersOrdersByYearAndMonth =
-                from cust in dataSource.Customers
+                from cust in this.dataSource.Customers
                 from ordrs in cust.Orders
                 group ordrs by new { Year = ordrs.OrderDate.Year, Month = ordrs.OrderDate.Month } into dateGroup
                 orderby dateGroup.Key.Year, dateGroup.Key.Month
@@ -301,4 +302,3 @@ namespace SampleQueries
         }
     }
 }
-
