1e8f6274a67865e1946e9f2a9bb05d2ef058d07a
Valeriy Satkov <valeriy.satkov@gmail.com>
2019-08-21 00:44:53 +0300

    Wrote BlockedMoneysServiceTests.cs and dependent classes

--- HW6/Valeriy_Satkov/Trading/Traiding.Core.Tests/BlockedMoneysServiceTests.cs
+++ HW6/Valeriy_Satkov/Trading/Traiding.Core.Tests/BlockedMoneysServiceTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Traiding.Core.Models;
+using Traiding.Core.Repositories;
+using Traiding.Core.Services;
 
 namespace Traiding.Core.Tests
 {
@@ -7,21 +11,134 @@ namespace Traiding.Core.Tests
     public class BlockedMoneysServiceTests
     {
         [TestMethod]
-        public void ShouldAddNewItem()
+        public void ShouldCreateNewBlockedMoneyItem()
         {
-            throw new NotImplementedException();
-        }
-        
-        [TestMethod]
-        public void ShouldGetItemInfo()
-        {
-            throw new NotImplementedException();
+            // Arrange
+            var blockedMoneyTableRepository = Substitute.For<IBlockedMoneyTableRepository>();
+            BlockedMoneysService blockedMoneysService = new BlockedMoneysService(blockedMoneyTableRepository);
+            BlockedMoneyRegistrationInfo args = new BlockedMoneyRegistrationInfo();
+            args.ClientBalance = new BalanceEntity()
+            {
+                Id = 45,
+                Client = new ClientEntity()
+                {
+                    Id = 5,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "John",
+                    LastName = "Snickers",
+                    PhoneNumber = "+7956244636652",
+                    Status = true
+                },
+                Amount = 20000.00M,
+                Status = true
+            };
+            args.Operation = new OperationEntity()
+            {
+                Id = 2
+            };
+            args.Total = 10000.00M;
+
+            // Act
+            var blockedMoneyId = blockedMoneysService.Create(args);
+
+            // Assert
+            blockedMoneyTableRepository.Received(1).Add(Arg.Is<BlockedMoneyEntity>(
+                bm => bm.ClientBalance == args.ClientBalance
+                && bm.Operation == args.Operation
+                && bm.Total == args.Total));
+            blockedMoneyTableRepository.Received(1).SaveChanges();
         }
 
         [TestMethod]
-        public void ShouldRemoveItem()
+        [ExpectedException(typeof(ArgumentException), "I didn't get exception it's wrong!")]
+        public void ShouldNotRegisterNewBlockedMoneyItemIfItExists()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var blockedMoneyTableRepository = Substitute.For<IBlockedMoneyTableRepository>();
+            BlockedMoneysService blockedMoneysService = new BlockedMoneysService(blockedMoneyTableRepository);
+            BlockedMoneyRegistrationInfo args = new BlockedMoneyRegistrationInfo();
+            args.ClientBalance = new BalanceEntity()
+            {
+                Id = 45,
+                Client = new ClientEntity()
+                {
+                    Id = 5,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "John",
+                    LastName = "Snickers",
+                    PhoneNumber = "+7956244636652",
+                    Status = true
+                },
+                Amount = 20000.00M,
+                Status = true
+            };
+            args.Operation = new OperationEntity()
+            {
+                Id = 2
+            };
+            args.Total = 10000.00M;
+
+            // Act
+            blockedMoneysService.Create(args);
+
+            blockedMoneyTableRepository.Contains(Arg.Is<BlockedMoneyEntity>( // Now Contains returns true (table contains blocked money with this data)
+                bm => bm.ClientBalance == args.ClientBalance
+                && bm.Operation == args.Operation
+                && bm.Total == args.Total)).Returns(true);
+
+            blockedMoneysService.Create(args); // Try to reg. same twice and get exception
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ShouldGetBlockedMoneyItemInfo()
+        {
+            // Arrange
+            var blockedMoneyTableRepository = Substitute.For<IBlockedMoneyTableRepository>();
+            int testId = 55;
+            blockedMoneyTableRepository.ContainsById(Arg.Is(testId)).Returns(true);
+            BlockedMoneysService blockedMoneysService = new BlockedMoneysService(blockedMoneyTableRepository);            
+
+            // Act
+            var sharesNumberInfo = blockedMoneysService.Get(testId);
+
+            // Assert
+            blockedMoneyTableRepository.Received(1).Get(testId);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "I didn't get exception it's wrong!")]
+        public void ShouldThrowExceptionIfCantFindSpecificBlockedMoneyItem()
+        {
+            // Arrange
+            var blockedMoneyTableRepository = Substitute.For<IBlockedMoneyTableRepository>();
+            int testId = 55;
+            blockedMoneyTableRepository.ContainsById(Arg.Is(testId)).Returns(false); // Now Contains returns false (table don't contains blocked money with this Id)
+            BlockedMoneysService blockedMoneysService = new BlockedMoneysService(blockedMoneyTableRepository);
+
+            // Act
+            blockedMoneysService.ContainsById(testId); // Try to get blocked money and get exception
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ShouldRemoveBlockedMoneyItem()
+        {
+            // Arrange
+            var blockedMoneyTableRepository = Substitute.For<IBlockedMoneyTableRepository>();
+            int testId = 55;
+            blockedMoneyTableRepository.ContainsById(Arg.Is(testId)).Returns(true);
+            BlockedMoneysService blockedMoneysService = new BlockedMoneysService(blockedMoneyTableRepository);
+
+            // Act
+            blockedMoneysService.Remove(testId);
+
+            // Assert
+            blockedMoneyTableRepository.Received(1).Remove(testId);
         }
     }
+
+    
 }
---
+++ HW6/Valeriy_Satkov/Trading/Traiding.Core/Repositories/IBlockedMoneyTableRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traiding.Core.Models;
+
+namespace Traiding.Core.Repositories
+{
+    public interface IBlockedMoneyTableRepository
+    {
+        bool Contains(BlockedMoneyEntity entity); // Compare by 
+        void Add(BlockedMoneyEntity entity);
+        void SaveChanges();
+        bool ContainsById(int entityId);
+        BlockedMoneyEntity Get(int entityId);
+        void Remove(int entityId);
+    }
+}
