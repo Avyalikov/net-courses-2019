68a20b092091b2f305c22941ddac4cb9cf03d45a
spore9 <spore9@yandex.ru>
2019-08-29 01:17:16 +0300

    Initial commit

---
+++ HW7/Kirill_Uryvaev/Trading/Trading.WebApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace Trading.WebApp
+{
+    public class Startup
+    {
+        // This code configures Web API contained in the class Startup, which is additionally specified as the type parameter in WebApplication.Start
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "ControllersApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
