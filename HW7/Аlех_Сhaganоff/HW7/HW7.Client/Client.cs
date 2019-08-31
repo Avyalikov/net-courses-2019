95447b2ae268557b16ad04f62f3a38383473de47
alch-0 <52721901+alch-0@users.noreply.github.com>
2019-08-31 20:30:29 +0300

    Complete solution (core, server, client)

---
+++ HW7/Аlех_Сhaganоff/HW7/HW7.Client/Client.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7.Client
+{
+    public class Client
+    {
+        private readonly Requests requests;
+
+        public Client(Requests requests)
+        {
+            this.requests = requests;
+        }
+
+        public void Run()
+        {
+            bool exitCondition = false;
+            int userChoice = 0;
+            List<int> menuChoices = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0 };
+            
+            requests.Get(requests.connectionString);
+
+            do
+            {
+                DisplayMenu();
+                userChoice = 0;
+
+                userChoice = ProcessUserInput(menuChoices);
+
+                if (userChoice == 0)
+                {
+                    exitCondition = true;
+                }
+                else
+                {
+                    CallFunction(userChoice);
+                }
+            }
+            while (!exitCondition);
+        }
+
+        public void CallFunction(int userChoice)
+        {
+            switch (userChoice)
+            {
+                case 1: requests.GetListOfClients(); break;
+                case 2: requests.AddClient(); break;
+                case 3: requests.UpdateClient(); break;
+                case 4: requests.RemoveClient(); break;
+                case 5: requests.GetListOfShares();  break;
+                case 6: requests.AddShare(); break;
+                case 7: requests.UpdateShare(); break;
+                case 8: requests.RemoveShare(); break;
+                case 9: requests.GetBalance(); break;
+                case 10: requests.GetTransactions(); break;
+                case 11: requests.MakeDeal(); break;
+            }
+        }
+
+        public int ProcessUserInput(List<int> menuChoices)
+        {
+            int inputValue = 0;
+            bool inputCheck = true;
+
+            do
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    inputValue = Convert.ToInt32(input);
+
+                    if (menuChoices.Contains(inputValue))
+                    {
+                        inputCheck = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose one of the numbers on the screen");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect input");
+                    Console.WriteLine("Please enter a single integer value");
+                }
+            }
+            while (inputCheck == false);
+
+            return inputValue;
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("1-Get list of clients");
+            Console.WriteLine("2-Add client");
+            Console.WriteLine("3-Update client");
+            Console.WriteLine("4-Remove client");
+            Console.WriteLine("5-Get list of shares");
+            Console.WriteLine("6-Add share");
+            Console.WriteLine("7-Update share");
+            Console.WriteLine("8-Remove share");
+            Console.WriteLine("9-Get balance");
+            Console.WriteLine("10-Get transactions");
+            Console.WriteLine("11-Make deal");
+            Console.WriteLine("0-Exit");
+        }
+    }
+}
---
+++ HW7/Аlех_Сhaganоff/HW7/HW7.Server/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HW7.Core;
+using HW7.Core.Repositories;
+using HW7.Core.Services;
+using HW7.Server.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HW7.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddScoped<IContextProvider>(_ => new TradingContext(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<ITradersRepository>(_ => new TradersRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.AddSingleton<ISharesRepository>(_ => new SharesRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.AddSingleton<ITransactionsRepository>(_ => new TransactionsRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.AddSingleton<TradersService>(_ => new TradersService(new TradersRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection")))));
+            services.AddSingleton<SharesService>(_ => new SharesService(new SharesRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection")))));
+            services.AddSingleton<TransactionsService>(_ => new TransactionsService(
+                new TransactionsRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))),
+                new TradersRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))),
+                new SharesRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection"))),
+                new PortfoliosRepository(new TradingContext(Configuration.GetConnectionString("DefaultConnection")))
+                ));
+        }
+        #endregion 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
