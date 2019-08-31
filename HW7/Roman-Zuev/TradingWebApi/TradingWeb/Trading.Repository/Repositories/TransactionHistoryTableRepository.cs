51373c5de8d05a3ccb5602e501006650558a95f5
hobowithshotgun <strochwow12@gmail.com>
2019-08-31 14:23:34 +0300

    in progress

---
+++ HW7/Roman-Zuev/TradingWebApi/TradingWeb/Trading.Repository/Repositories/TransactionHistoryTableRepository.cs
@@ -0,0 +1,31 @@
+﻿using Trading.Repository.Context;
+
+namespace Trading.Repository.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using Trading.Core.Models;
+    using Trading.Core.Repositories;
+    using Trading.Repository.Context;
+    using System.Linq;
+    public class TransactionHistoryTableRepository : ITransactionHistoryTableRepository
+    {
+        private readonly TradesEmulatorDbContext dbContext;
+
+        public TransactionHistoryTableRepository (TradesEmulatorDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Add(TransactionHistoryEntity transaction)
+        {
+            this.dbContext.TransactionHistories.Add(transaction);
+        }
+
+        public void SaveChanges()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
