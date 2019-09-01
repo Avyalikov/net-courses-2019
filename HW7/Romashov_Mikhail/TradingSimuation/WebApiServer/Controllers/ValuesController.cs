4087bb982a4a2522e6867d019331dd9171a607fd
Mikhail Romashov <romashovmvl@gmail.com>
2019-09-01 10:38:08 +0300

    Added first httpget to db

---
+++ HW7/Romashov_Mikhail/TradingSimuation/WebApiServer/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TradingSimulator.Core.Services;
+
+namespace WebApiServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ITraderService tradersService;
+
+        public ValuesController(ITraderService tradersService)
+        {
+            this.tradersService = tradersService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            List<int> ids = tradersService.GetList();
+            return JsonConvert.SerializeObject(ids);
+        }
+
+
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"value + {id}";
+        }
+
+
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>() { "value1", "value2" };
+        }
+
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"value - {id}";
+        }
+
+
+    }
+}
---
+++ HW7/Romashov_Mikhail/TradingSimuation/WebApiServer/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using System.Configuration;
+using TradingSimulator.Core.Repositories;
+using TradingSimulator.Core.Services;
+using WebApiServer.Repositories;
+
+namespace WebApiServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void ConfigureContainer(Registry registry)
+        {
+            registry.For<ITraderTableRepository>().Use<TraderTableRepository>();
+            registry.For<IStockTableRepository>().Use<StockTableRepository>();
+            registry.For<IHistoryTableRepository>().Use<HistoryTableRepository>();
+            registry.For<ITraderStockTableRepository>().Use<TraderStockTableRepository>();
+            registry.For<ITraderService>().Use<TradersService>();
+            registry.For<TradingSimulatorDBContext>().Use<TradingSimulatorDBContext>().Ctor<string>("connectionString").Is(ConfigurationManager.ConnectionStrings["tradingSimulatorConnectionString"].ConnectionString);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
