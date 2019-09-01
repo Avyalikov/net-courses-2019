47d88192177dc49c30b290f742d4822a3b6a2d02
Valeriy Satkov <valeriy.satkov@gmail.com>
2019-09-02 00:26:44 +0300

    Remove Traiding.WebAPI project (IIS server)

--- HW7/Valeriy_Satkov/Trading/Traiding.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Traiding.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- HW7/Valeriy_Satkov/Trading/Traiding.WebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Traiding.WebAPI.WebApiApplication" Language="C#" %>
