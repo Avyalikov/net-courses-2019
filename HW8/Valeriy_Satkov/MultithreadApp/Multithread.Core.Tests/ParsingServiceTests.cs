2514cf4b2584c1994d4eec014f2257a7b2b1d921
Valeriy Satkov <valeriy.satkov@gmail.com>
2019-09-02 18:14:10 +0300

    Add empty classes into Core and Core.Tests projects

---
+++ HW8/Valeriy_Satkov/MultithreadApp/Multithread.Core.Tests/ParsingServiceTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Multithread.Core.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ParsingServiceTests
+    {
+        [TestMethod]
+        public void ShouldDownloadPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldExtractHtmlTags()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldSaveTagsIntoDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldCallParsingForEachPageFromPreviousIteration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
