5750877ef58fef4611d056efd0c90b1dc5cd08de
spore9 <spore9@yandex.ru>
2019-09-05 06:09:56 +0300

    First realization of multithreading

--- HW8/Kirill_Uryvaev/WikiURLCollector/WikiURLCollector.ConsoleApp/Program.cs
+++ HW8/Kirill_Uryvaev/WikiURLCollector/WikiURLCollector.ConsoleApp/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using WikiURLCollector.Core.Services;
 using WikiURLCollector.Core.Models;
 using StructureMap;
+using System.Diagnostics;
+using System.Threading;
 
 namespace WikiURLCollector.ConsoleApp
 {
@@ -19,32 +21,77 @@ namespace WikiURLCollector.ConsoleApp
             var urlService = container.GetInstance<UrlService>();
             string exitCode = "e";
             string userInput = "";
+            int maxIterations = 5;
             Console.WriteLine($"{DateTime.Now} Program started");
             while (!userInput.ToLower().Equals(exitCode))
             {
                 userInput = Console.ReadLine();
+                GetUrls(userInput, parsingService, urlService, maxIterations);
+            }
+        }
+
+        static async void GetUrls(string userInput, UrlParsingService parsingService, UrlService urlService, int maxIterations)
+        {
+            List<Task<IEnumerable<UrlEntity>>> tasks = new List<Task<IEnumerable<UrlEntity>>>();
+            List<Task<IEnumerable<UrlEntity>>> nextTasks = new List<Task<IEnumerable<UrlEntity>>>();
+            CancellationToken token = new CancellationToken();
+            int i = 1;
+            tasks.Add(new Task<IEnumerable<UrlEntity>>(()=> { return LoadPage(userInput, parsingService, i).Result; }, token));
+            Stopwatch watch = new Stopwatch();
+            string baseAdress = "https://en.wikipedia.org/";
+            Console.WriteLine($"{DateTime.Now} Start url parsing");
+            while (tasks.Count > 0 && i <= maxIterations)
+            {
+                watch.Restart();
+                tasks.ForEach(t => t.Start());
+                await Task.WhenAll(tasks);
+                watch.Stop();
+                if (tasks.Any(t => t.Exception != null))
+                {
+                    Console.WriteLine("Error has occurred");
+                    return;
+                }
+                Console.WriteLine($"Iteration {i} is completed in {watch.Elapsed}");
+
+                i++;
+                foreach (var task in tasks)
+                {
+                    foreach (var url in task.Result)
+                    {
+                        if (urlService.GetUrl(url.URL) == null)
+                        {
+                            urlService.AddUrl(url);
+                            nextTasks.Add(new Task<IEnumerable<UrlEntity>>(() => { return LoadPage(baseAdress + url.URL, parsingService, i).Result; }, token));
+                        }
+                    }
+                }
+                tasks.Clear();
+                tasks = nextTasks;
+                nextTasks = new List<Task<IEnumerable<UrlEntity>>>();
+            }
+        }
+
+        static async Task<IEnumerable<UrlEntity>> LoadPage(string adress, UrlParsingService parsingService, int iteration)
+        {
+            IEnumerable<UrlEntity> result = null;
+            try
+            {
                 using (HttpClient client = new HttpClient())
                 {
-                    using (HttpResponseMessage response = client.GetAsync(userInput).Result)
+                    using (HttpResponseMessage response = client.GetAsync(adress).Result)
                     {
-                        Console.WriteLine(response.StatusCode);
                         if (response.IsSuccessStatusCode)
                         {
-                           var result = LoadPage(response, userInput, parsingService);
-                            foreach (var url in result.Result)
-                            {
-                                urlService.AddUrl(url);
-                            }
+                            var pageContents = await response.Content.ReadAsStringAsync();
+                            result = parsingService.ExtractAllUrlsFromPage(pageContents, iteration);
                         }
                     }
                 }
             }
-        }
-
-        static async Task<IEnumerable<UrlEntity>> LoadPage(HttpResponseMessage response, string adress, UrlParsingService parsingService)
-        {
-            var pageContents = await response.Content.ReadAsStringAsync();
-            var result = parsingService.ExtractAllUrlsFromPage(pageContents,1);
+            catch (AggregateException e)
+            {
+                Thread.Sleep(1000);
+            }
             return result;
         }
     }
--- HW8/Kirill_Uryvaev/WikiURLCollector/WikiURLCollector.Core/Services/UrlParsingService.cs
+++ HW8/Kirill_Uryvaev/WikiURLCollector/WikiURLCollector.Core/Services/UrlParsingService.cs
@@ -24,11 +24,28 @@ namespace WikiURLCollector.Core.Services
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(rawDocument);
             IEnumerable<HtmlNode> allUrls = document.DocumentNode.SelectNodes("//a[@href]");
-            var filtredUrls = allUrls.Where(a => a.Attributes["href"].Value.StartsWith("/wiki/"));
+            var filtredUrls = allUrls.Where(a=> a.Attributes["href"].Value.StartsWith("/wiki/")).Where((a)=>
+            {
+                if (a.ParentNode.Attributes["class"] != null)
+                {
+                    return a.ParentNode.Attributes["class"].Value.Equals("reference");
+                }
+                if (a.ParentNode.Attributes.Count()!=0)
+                {
+                    return false;
+                }
+                return true;
+            });
+            if (filtredUrls.Count() == 0)
+                return null;
+
             foreach (var url in filtredUrls)
             {
-                UrlEntity urlEntity = new UrlEntity() { URL = url.Attributes["href"].Value, IterationId = iteration };
-                wikiUrls.Add(urlEntity);
+                if (wikiUrls.Where(u=>u.URL == url.Attributes["href"].Value).Count()==0)
+                {
+                    UrlEntity urlEntity = new UrlEntity() { URL = url.Attributes["href"].Value, IterationId = iteration };
+                    wikiUrls.Add(urlEntity);
+                }
             }
             if (wikiUrls.Count() > 0)
                 return wikiUrls;
