828682bf940fb76c6ba26cc475d4ac128c6c0c73
hobowithshotgun <strochwow12@gmail.com>
2019-09-09 07:38:31 +0300

    minor fixes

--- HW8/Roman-Zuev/ReferenceCollectorApp/ReferenceCollectorApp/Program.cs
+++ HW8/Roman-Zuev/ReferenceCollectorApp/ReferenceCollectorApp/Program.cs
@@ -13,14 +13,6 @@ namespace ReferenceCollectorApp
             var container = new Container(new ReferenceCollectorRegistry());
             var program = container.GetInstance<IReferenceCollector>();
             program.Run();
-            //var table = new ReferencesTable(new Context.ReferenceCollectorDbContext());
-            //var service = new ReferenceCollectorService(table);
-            //var dic = service.AddRefsToDictionary(service.DownloadPage("/wiki/Victory_Day_(Malta)", @".\resources\").Result, 1);
-            //foreach (var item in dic)
-            //{
-            //    Console.WriteLine(item.Key);
-            //}
-            //Console.ReadLine();
         }
     }
 }
