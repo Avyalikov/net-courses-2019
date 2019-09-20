25b84dc51981bc7342c7a10e5ada89640c3cd914
Artem <avlozhechkin@gmail.com>
2019-09-20 14:04:12 +0300

    Added Swagger

---
+++ HW9/Artem_Lozhechkin/WebApi/Startup.cs
@@ -0,0 +1,52 @@
+﻿namespace TradingApp.WebApi
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using StructureMap;
+    using Swashbuckle.AspNetCore.Swagger;
+    using System;
+
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Trading Web API",
+                    Description = "Web API for trading."
+                });
+            });
+            var container = new Container();
+
+            container.Configure(config =>
+            {
+                config.AddRegistry(new WebApiRegistry());
+                config.Populate(services);
+            });
+
+            return container.GetInstance<IServiceProvider>();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Trading Web API v1");
+            });
+        }
+    }
+}
